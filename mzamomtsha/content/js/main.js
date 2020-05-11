38f018b1d91bbc75041db7699b6d5eb8c51ed4c5
Sakhile <sakhi.mpungose365@gmail.com>
2020-05-11 02:25:11 +0200

    Adding functionality to allow the active nav link to change appropriately the user scrolls

--- mzamomtsha/content/js/main.js
+++ mzamomtsha/content/js/main.js
@@ -1,21 +1,44 @@
 (function(){
 	var menuToggler = document.querySelector(".menu-toggler");
 	var flexMenu = document.querySelector(".flex-menu");
-	menuToggler.addEventListener("click", function(){
-		if(this.classList.contains("menu-toggler-close")){
-			this.classList.remove("menu-toggler-close");
-			flexMenu.classList.add("hidden");
-			flexMenu.classList.remove("shown");
+	var links =  document.querySelectorAll(".flex-menu-item, .dropdown-menu-link, .dropdown-menu-item");
+	var dropdowns = document.querySelectorAll(".dropdown-menu");
+	
+	var toggleMenu = function(){
+		flexMenu.style.display = (flexMenu.style.display || window.getComputedStyle(flexMenu).getPropertyValue("display")) == "flex" ? "none" : "flex";
+	};
+	
+	var toggleMenuToggler = function(){
+		if(menuToggler.classList.contains("menu-toggler-close")){
+			menuToggler.classList.remove("menu-toggler-close");
 		}else{
-			this.classList.add("menu-toggler-close");
-			flexMenu.classList.add("shown");
-			flexMenu.classList.remove("hidden");
+			menuToggler.classList.add("menu-toggler-close");
 		}
+	};
+	
+	menuToggler.addEventListener("click", function(e){
+		e.preventDefault();		
+		toggleMenu();
+		toggleMenuToggler();
+	}, false);
+	
+	var section = document.querySelectorAll(".section-marker");
+	var sections = {};
+	
+	Array.prototype.forEach.call(section, function(e){
+		sections[e.id] = e.offsetTop;
 	});
 	
-	var links =  document.querySelectorAll(".flex-menu-item, .dropdown-menu-link, .dropdown-menu-item");
+	window.onscroll = function(){
+		var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+		for(i in sections){
+			if(sections[i] <= scrollPosition){
+				document.querySelector(".active").classList.remove('active');
+				document.querySelector('a[href*=' + i +']').classList.add('active');
+			}
+		}
+	}
 	
-	var dropdowns = document.querySelectorAll(".dropdown-menu");
 	
 	for(linkElement of links){
 		linkElement.addEventListener("click", function(e){
